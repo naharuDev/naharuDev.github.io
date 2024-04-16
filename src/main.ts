a1721ca5a108d4420030835608b1e0c00042ca50
Ahmad Fadli Naharu <88329745+naharuDev@users.noreply.github.com>
2024-04-16 17:00:47 +0700

    new

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,28 @@
-import './style.css'
+import './tailwind.css'
 import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
+  <div class="bg-gradient-to-t from-cyan-950 to-sky-800">
+
+    <!-- 
     <a href="https://www.typescriptlang.org/" target="_blank">
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+    </a> 
+    -->
+
+    <h1 class="text-center pt-5" >Hi, Welcome</h1>
+    <article class="text-justify px-10 pt-5">
+      <p> 
+        My name is Ahmad Fadli Naharu, I am a Game Programmer. Mostly, I'm working using Unity3D Game Engine.
+        Other than my role as a Game Programmer, I love art both in visual and in audio form. In this Web Page,
+        I present my portofolios and my artworks. 
+      </p> 
+    </article>
+    
+    <div class="px-10 pt-5">
+      <button id="counter" type="button" class=" rounded-lg bg-sky-800" ></button>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
   </div>
 `
 
