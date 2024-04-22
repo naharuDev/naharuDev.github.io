4d88055206ec34a3e5d74b65e2b273ff98bcea22
Ahmad Fadli Naharu <88329745+naharuDev@users.noreply.github.com>
2024-04-22 20:22:06 +0700

    update

--- src/header.ts
+++ src/header.ts
@@ -2,6 +2,6 @@
 import backgroundImage from './img/Coffee.png'
 
 
-document.querySelector<HTMLDivElement>('#head')!.innerHTML = `
+document.querySelector<HTMLDivElement>('#nav')!.innerHTML = `
 <img src="${backgroundImage}" class="animate-fade object-cover bg-no-repeat"></img>
 `
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import './tailwind.css'
 import backgroundImage from './img/Coffee Line.png'
 import { setupCounter } from './counter.ts'
 
+
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div class ="h-auto w-full relative -z-30 overflow-hidden">
     <img src="${backgroundImage}" class="absolute object-center object-cover h-full w-full animate-fade">
@@ -11,9 +12,9 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
       </div>
     </img>
   </div>
-
+  
   <div class="bg-emerald-300 bg-opacity-10 z-20 overflow-hidden relative">
-    <div class="w-40 h-40 rounded-full bg-emerald-100 object-center m-10 absolute filter blur-xl -z-10"></div> 
+    <div class="w-40 h-40 rounded-full bg-emerald-100 opacity-40 object-center m-10 absolute filter blur-3xl -z-10 animate-bounce"></div> 
     <section class="px-10 pt-2 ">
       <div class="bg-emerald-50 h-1 sm:m-2 rounded-sm"></div>
       <h1 class="text-3xl sm:text-4xl md:text-5xl lg:text-6xl text-left px-15 lg:mx-20">LET'S COLABORATE<br> AND MAKE <br> SOMETHING GREAT</h1>
@@ -25,10 +26,22 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 
     <section class ="px-10 py-20">
       <div class="bg-emerald-50 h-1 sm:m-2 rounded-sm "></div>
-      <h1 class = "p-10 text-4xl">3D Animation</h1>
+      <h1 class = "p-10 text-4xl">Animation</h1>
       <div class ="bg-lime-50 bg-opacity-20 h-10 "></div>
     </section>
-    
+
+    <section class ="px-10 py-20">
+      <div class="bg-emerald-50 h-1 sm:m-2 rounded-sm "></div>
+      <h1 class = "p-10 text-4xl">Painting</h1>
+      <div class ="bg-lime-50 bg-opacity-20 h-10 "></div>
+    </section>
+
+    <section class ="px-10 py-20 ">
+      <div class="bg-emerald-50 h-1 sm:m-2 rounded-sm "></div>
+      <h1 class = "p-10 text-4xl">Game Development</h1>
+      <div class ="bg-lime-50 bg-opacity-20 h-10 "></div>
+    </section>
+
     <div class="px-10 pt-5">
       <button id="counter" type="button" class=" rounded-lg bg-sky-800" ></button>
     </div>
